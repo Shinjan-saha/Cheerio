4e3ba54b6d0de3514e8644ef65da3dce2edb1741
Shinjan-saha <shinjansaha02@gmail.com>
2023-12-25 20:44:31 +0530

    analytics

---
+++ index.js
@@ -0,0 +1,8 @@
+// Import the analytics package
+import analytics from '@vercel/analytics';
+
+// Initialize the analytics package with your project ID
+analytics({
+  // Add your Vercel project ID here
+  projectId: 'cheerio-ece-farewell',
+});
